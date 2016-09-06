099c57ff71ccab43e5b37350f290b4d7f2ae1178
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-09-06 22:01:20 +0300

    описаны все функции

--- bars.py
+++ bars.py
@@ -2,20 +2,43 @@ import json
 
 
 def load_data(filepath):
-    pass
-
+    data =[]
+    with open(filepath) as data_file:    
+        data = json.load(data_file)
+    return data
 
 def get_biggest_bar(data):
-    pass
+    seat_count = data[0]['Cells']['SeatsCount']
+    for bar in data:
+        if bar['Cells']['SeatsCount']>seat_count:
+            seat_count = bar['Cells']['SeatsCount']
+            biggest_bar = bar['Cells']['Name']
+    return biggest_bar
 
 
 def get_smallest_bar(data):
-    pass
+    seat_count = data[0]['Cells']['SeatsCount']
+    for bar in data:
+        if bar['Cells']['SeatsCount']<seat_count:
+            seat_count = bar['Cells']['SeatsCount']
+            smallest_bar = bar['Cells']['Name']
+    return smallest_bar
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
-
+    difference = abs(longitude-data[0]['Cells']['geoData']['coordinates'][0]) + abs(latitude-data[0]['Cells']['geoData']['coordinates'][1])
+    closest_bar = data[0]['Cells']['Name']
+    for bar in data:
+        if (abs(longitude-bar['Cells']['geoData']['coordinates'][0])+abs(latitude-bar['Cells']['geoData']['coordinates'][1])) < difference:
+            difference = abs(longitude-bar['Cells']['geoData']['coordinates'][0])+abs(latitude-bar['Cells']['geoData']['coordinates'][1])
+            closest_bar = bar['Cells']['Name']
+    return closest_bar    
+            
 
 if __name__ == '__main__':
-    pass
+    #проверка работоспособности
+    print(get_biggest_bar(load_data('bars.json')))
+    print(get_smallest_bar(load_data('bars.json')))
+    longitude = float(input('Input your gps coordinates(longitude):'))
+    latitude = float(input('Input your gps coordinates(latitude):'))
+    print(get_closest_bar(load_data('bars.json'),longitude,latitude))
