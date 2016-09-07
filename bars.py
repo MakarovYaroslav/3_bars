8bf648af42c55b47ddc1837fcd1849e162b09bf4
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-09-07 20:33:18 +0300

    исправлены ошибки, добавлена ясность в print

--- bars.py
+++ bars.py
@@ -2,15 +2,16 @@ import json
 
 
 def load_data(filepath):
-    data =[]
+    data = []
     with open(filepath) as data_file:    
         data = json.load(data_file)
     return data
 
+
 def get_biggest_bar(data):
     seat_count = data[0]['Cells']['SeatsCount']
     for bar in data:
-        if bar['Cells']['SeatsCount']>seat_count:
+        if bar['Cells']['SeatsCount'] > seat_count:
             seat_count = bar['Cells']['SeatsCount']
             biggest_bar = bar['Cells']['Name']
     return biggest_bar
@@ -19,7 +20,7 @@ def get_biggest_bar(data):
 def get_smallest_bar(data):
     seat_count = data[0]['Cells']['SeatsCount']
     for bar in data:
-        if bar['Cells']['SeatsCount']<seat_count:
+        if bar['Cells']['SeatsCount'] < seat_count:
             seat_count = bar['Cells']['SeatsCount']
             smallest_bar = bar['Cells']['Name']
     return smallest_bar
@@ -32,13 +33,13 @@ def get_closest_bar(data, longitude, latitude):
         if (abs(longitude-bar['Cells']['geoData']['coordinates'][0])+abs(latitude-bar['Cells']['geoData']['coordinates'][1])) < difference:
             difference = abs(longitude-bar['Cells']['geoData']['coordinates'][0])+abs(latitude-bar['Cells']['geoData']['coordinates'][1])
             closest_bar = bar['Cells']['Name']
-    return closest_bar    
-            
+    return closest_bar
+    
 
 if __name__ == '__main__':
-    #проверка работоспособности
-    print(get_biggest_bar(load_data('bars.json')))
-    print(get_smallest_bar(load_data('bars.json')))
-    longitude = float(input('Input your gps coordinates(longitude):'))
-    latitude = float(input('Input your gps coordinates(latitude):'))
-    print(get_closest_bar(load_data('bars.json'),longitude,latitude))
+    bars_info = load_data('bars.json')
+    print('Самый большой бар - %s' % get_biggest_bar(bars_info))
+    print('Самый маленький бар - %s' % get_smallest_bar(bars_info))
+    longitude = float(input('Введи свои GPS-координаты (долготу):'))
+    latitude = float(input('Введи свои GPS-координаты (широту):'))
+    print('Самый близкий к тебе бар - %s' % get_closest_bar(bars_info, longitude, latitude))
